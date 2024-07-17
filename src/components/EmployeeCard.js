bc814cb6b21893b2711e2d8952804f54a6ab5b15
Chad96 <chadrackndalamba@gmail.com>
2024-07-17 09:42:19 +0200

    image updated

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -1,10 +1,24 @@
-// src/components/EmployeeCard.js
 import React from "react";
 
 const EmployeeCard = ({ employee, onEdit, onDelete }) => {
+  const isExternalUrl = (url) => {
+    try {
+      return new URL(url).origin !== window.location.origin;
+    } catch {
+      return false;
+    }
+  };
+
+  const getImageSrc = (image) => {
+    return isExternalUrl(image) ? image : `${process.env.PUBLIC_URL}/${image}`;
+  };
+
   return (
     <div className="employee-card">
-      <img src={employee.image} alt={`${employee.name} ${employee.surname}`} />
+      <img
+        src={getImageSrc(employee.image)}
+        alt={`${employee.name} ${employee.surname}`}
+      />
       <h2>
         {employee.name} {employee.surname}
       </h2>
