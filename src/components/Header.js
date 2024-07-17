344980df867c18d154dc65f8ade2d3e638aeb7d4
Chad96 <chadrackndalamba@gmail.com>
2024-07-17 12:48:04 +0200

    cards updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
-// src/components/Header.js
 import React from "react";
+import "../styles.css"; // Ensure to import your CSS file
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Header = ({ searchTerm, setSearchTerm }) => {
   return (
     <header>
       <h1>Employee Management Portal</h1>
-      <input
-        type="text"
-        placeholder="Search employees..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+      <div className="search-container">
+        <FontAwesomeIcon icon={faSearch} className="search-icon" />
+        <input
+          type="text"
+          placeholder="Search employees..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
     </header>
   );
 };
